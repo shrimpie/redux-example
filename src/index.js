9d38492b2835658c608e220073f5148d66d3d8c3
杨振宇 <andy@young.local>
2017-01-09 13:25:34 -0500

    Combined all reducers

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
 import C from './constants'
 // What does C mean here?
+import appReducer from './store/reducers'
+import initialState from './initialState.json'
 
-import { allSkiDays } from './store/reducers'
-
-const state = [
-	{
-		"resort": "Kirkwood",
-		"date": "2016-12-15",
-		"powder": true,
-		"backcountry": false
-	},
-	{
-		"resort": "Boreal",
-		"date": "2016-12-16",
-		"powder": false,
-		"backcountry": false
-	}
-]
-
-const action = {
-	type: C.REMOVE_DAY,
-	payload: "2016-12-15"
-}
-
-const nextState = allSkiDays(state, action)
+let state = initialState
 
 console.log(`
 
-    initial goal: ${JSON.stringify(state)}
-    action: ${JSON.stringify(action)}
-    new goal: ${JSON.stringify(nextState)}
+	Initial state
+	=============
+	goal: ${state.goal}
+	resorts: ${JSON.stringify(state.allSkiDays)}
+	fetching: ${state.resortNames.fetching}
+	suggestions: ${state.resortNames.suggestions}
+
+`)
+
+state = appReducer(state, {
+	type: C.SET_GOAL,
+	payload: 2
+})
+
+state = appReducer(state, {
+	type: C.ADD_DAY,
+	payload: {
+		"resort": "Mt Shasta",
+		"date": "2016-10-28",
+		"powder": false,
+		"backcountry": true
+	}
+})
+
+state = appReducer(state, {
+	type: C.CHANGE_SUGGESTIONS,
+	payload: ["Mt Tallac", "Mt Hood", "Mt Shasta"]
+})
+
+console.log(`
+
+	Next state
+	=============
+	goal: ${state.goal}
+	resorts: ${JSON.stringify(state.allSkiDays)}
+	fetching: ${state.resortNames.fetching}
+	suggestions: ${state.resortNames.suggestions}
 
 `)
